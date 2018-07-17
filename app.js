0c74a93d6a88ac4796334cca843f60241212f817
prateekjain <mrprateekjain@gmail.com>
2018-07-17 15:11:51 +0530

    appid and password for microsoft chatbot

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const server = express();
 });*/
 
 var connector = new builder.ChatConnector({
-    appId: null,
-    appPassword: null
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 server.get(`/`, (_, res) => res.sendFile(path.join(__dirname + '/index.html')));
