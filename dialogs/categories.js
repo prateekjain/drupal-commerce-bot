a9a25c0dd82ffc7e629e8738114ced0b2a8f9f69
prateekjain <mrprateekjain@gmail.com>
2018-07-19 17:35:02 +0530

    Added speech locale

--- dialogs/categories.js
+++ dialogs/categories.js
@@ -1,4 +1,6 @@
 const builder = require('botbuilder');
+var ssml = require('../api/ssml');
+
 module.exports = function (bot) {
   bot.dialog('/categories', [
     function (session, args, next) {
@@ -6,11 +8,12 @@ module.exports = function (bot) {
 
       session.sendTyping();
 
-      session.send(
+      session.endDialog(
         listCategoriesCard(session)
       );
 
-      session.endDialog('Choose what you want');
+      //session.say("This output speech uses SSML.","<speak>This output speech uses SSML.</speak>");
+      //session.endDialog('Choose what you want');
     }
   ]);
 };
@@ -23,6 +26,7 @@ function listCategoriesCard(session) {
       "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
       "type": "AdaptiveCard",
       "version": "1.0",
+      "speak":"<speak><audio src='https://www.soundjay.com/misc/bell-ringing-04.mp3'/><s>Time to wake up!</s></speak>",
       "body": [
         {
           "type": "Container",
@@ -89,8 +93,18 @@ function listCategoriesCard(session) {
     }
   };
 
+    
   var adaptiveCardMessage = new builder.Message(session)
+    .speak(speak(session, 'show_category_ssml'))
+    .inputHint(builder.InputHint.acceptingInput)
     .addAttachment(card);
 
   return adaptiveCardMessage;
+}
+
+/** Helper function to wrap SSML stored in the prompts file with <speak/> tag. */
+function speak(session, prompt) {
+    var localized = session.gettext(prompt);
+    console.log(localized);
+    return ssml.speak(localized);
 }
